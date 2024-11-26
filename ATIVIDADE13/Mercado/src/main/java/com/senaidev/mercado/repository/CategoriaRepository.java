ca25697dbe606af66bead966a3fa35a32b8eb493
Júlio César Botaccio <159179841+jwlioo@users.noreply.github.com>
2024-11-26 07:58:37 -0300

    Delete ATIVIDADE13 directory

--- ATIVIDADE13/Mercado/src/main/java/com/senaidev/mercado/repository/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.senaidev.mercado.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.senaidev.mercado.entities.Categoria;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
-
-}
--- ATIVIDADE13/Mercado/src/main/java/com/senaidev/mercado/repository/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.senaidev.mercado.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.senaidev.mercado.entities.Produto;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long>{
-
-}
